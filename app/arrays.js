7f3496efe51869b8b916a98cfdbbee3f293494ae
MKS-Student <mks.student@hackreactor.com>
2016-01-27 11:15:30 -0800

    started on arrays

--- app/arrays.js
+++ app/arrays.js
@@ -3,19 +3,39 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] === item) {
+        return i;
+      } 
+    }
+    return -1;
   },
 
   sum : function(arr) {
-
+    var total = 0;
+    for(var i = 0; i < arr.length; i++) {
+      total = total + arr[i];
+    }
+    return total;
   },
 
   remove : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] === item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
-
+    var newArr = arr;
+    for(var i = 0; i < arr.length; i++) {
+      if(arr[i] === item) {
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   append : function(arr, item) {
